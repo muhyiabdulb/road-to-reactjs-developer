1bfc408846005bd25157ebad64f7bf0094bd867e
muhyiabdulb <muhyiabdulbasith660@gmail.com>
2021-08-10 10:16:03 +0700

    create component & dynamic data

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import RecipeTitle from './RecipeTitle'
+
+function App(){
+    return(
+        <article>
+            <h1>Recipe Manager</h1>
+            <RecipeTitle /> 
+        </article>
+    )
+}
+
+export default App
--- src/index.jsx
+++ src/index.jsx
@@ -1,14 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
+import './index.css';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-  // <React.StrictMode>
-  //   <App />
-  // </React.StrictMode>,
-  <h1>Hello, World</h1>,
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
   document.getElementById('root')
 );
 
